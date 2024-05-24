e9f7984a386e579926231d2a8c2a36bf5e468e59
Marcos Santos <marcossantosmsms604@gmail.com>
2024-05-24 16:22:12 -0300

    Projeto criado

---
+++ src/test/java/com/marcos/api_pedidos/ApiPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.api_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
